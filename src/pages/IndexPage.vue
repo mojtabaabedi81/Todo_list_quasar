2df97dcf301ae883940e9efc7c6ff239d10d949b
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2024-02-21 12:38:39 +0330

    the first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page padding>
+    <input  v-model="message"/>
+    <button @click="message = ''">clear</button>
+    <h5>
+      {{message}}
+    </h5>
+  </q-page>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  data(){
+    return {
+      message:'i love vue js! much harder than you thought'
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
