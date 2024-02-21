efd1c1a75cba9a863e45f1cd70719d68e793c99f
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2024-02-21 15:35:23 +0330

    add some new elements like the button for the clear and the others

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,29 @@
 <template>
   <q-page padding>
-    <input  v-model="message"/>
-    <button @click="message = ''">clear</button>
-    <h5>
-      {{message}}
-    </h5>
+    <button style="position: absolute; right: 10px"
+            @click="counter++"
+    >
+      {{ counter }}
+    </button>
+
+    <input
+      v-model="message"
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"
+      v-autofocus
+    />
+
+    <button @click="clearMessage">clear</button>
+    <h5
+      v-if="message.length"
+      class="border-grey">{{ message }}</h5>
+    <h6 v-else>No message entered 😒</h6>
+
+    <hr>
+
+    <p>Uppercase Message: {{ messageUppercase }}</p>
+    <p>lowerCase message:{{ messageLowercase }} </p>
+
   </q-page>
 </template>
 
@@ -12,14 +31,41 @@
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  data(){
+  data() {
     return {
-      message:'i love vue js! much harder than you thought'
+      message: 'i love vue js! much harder than you thought',
+      counter: 0
+    }
+  },
+  computed: {
+    messageUppercase() {
+      console.log('messageUppercase was fired')
+      return this.message.toUpperCase() + this.counter
+    },
+    messageLowercase() {
+      return this.message.toLowerCase()
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = ''
+    },
+    alertMessage() {
+      alert(this.message)
+    }
+  },
+  directives :{
+    autofocus : {
+      inserted(el){
+        el.focus()
+      }
     }
   }
 })
 </script>
 
 <style>
-
+.border-grey {
+  border: 3px solid grey;
+}
 </style>
