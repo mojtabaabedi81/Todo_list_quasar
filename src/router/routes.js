61f413091de7290e91c03bab90b74af0153f7eb3
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2024-02-24 14:11:49 +0330

    add some route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,16 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/Layout.vue'),
     children: [
-      {path: '', component: () => import('pages/IndexPage.vue')}
+      {
+        path: '',
+        component: () => import('pages/PageTodo.vue')
+      },
+      {
+        path: '/settings',
+        component: () => import('pages/PageSettings.vue')
+      }
     ]
   },
 
