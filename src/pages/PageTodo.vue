cbd74ae5ff5559df7c9b86888b5682f40466d752
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2024-03-03 12:55:06 +0330

    it already have the component issue

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -3,56 +3,67 @@
     <q-list
       separator
       bordered>
-      <q-item
-        v-for="(task) in tasks"
-        :key="task.id"
-        @click="task.completed =!task.completed"
-        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-        clickable
-        v-ripple>
-        <q-item-section side top>
-          <q-checkbox v-model="task.completed"/>
-        </q-item-section>
+      <task
+        v-for="(task,key) in tasks"
+        :key="key"
+        :task="task"
+        :id="key"
+      ></task>
+      <!--      <q-item-->
+      <!--        v-for="(task,key) in tasks"-->
+      <!--        :key="key"-->
+      <!--        @click="task.completed =!task.completed"-->
+      <!--        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"-->
+      <!--        clickable-->
+      <!--        v-ripple>-->
+      <!--        <q-item-section side top>-->
+      <!--          <q-checkbox v-model="task.completed"/>-->
+      <!--        </q-item-section>-->
 
-        <q-item-section>
-          <q-item-label
-            :class="{'text-strikethrough' : task.completed}"
-          >{{ task.name }}
-          </q-item-label>
-        </q-item-section>
-        <q-item-section side>
-          <div class="row">
-            <div class="column justify-center">
-              <q-icon
-                name="event"
-                size="18px"
-                class="q-mr-xs"
-              />
-            </div>
+      <!--        <q-item-section>-->
+      <!--          <q-item-label-->
+      <!--            :class="{'text-strikethrough' : task.-->
+      <!--            completed}"-->
+      <!--          >{{ task.name }} {{key}}-->
+      <!--          </q-item-label>-->
+      <!--        </q-item-section>-->
+      <!--        <q-item-section side>-->
+      <!--          <div class="row">-->
+      <!--            <div class="column justify-center">-->
+      <!--              <q-icon-->
+      <!--                name="event"-->
+      <!--                size="18px"-->
+      <!--                class="q-mr-xs"-->
+      <!--              />-->
+      <!--            </div>-->
 
-            <div class="column">
-              <q-item-label
-                class="row justify-end"
-                caption>{{ task.dueDate }}
-              </q-item-label>
-              <q-item-label
-                caption
-                class="row justify-end">
-                <small>{{ task.dueTime }}</small>
-              </q-item-label>
-            </div>
-          </div>
-        </q-item-section>
-      </q-item>
+      <!--            <div class="column">-->
+      <!--              <q-item-label-->
+      <!--                class="row justify-end"-->
+      <!--                caption>{{ task.dueDate }}-->
+      <!--              </q-item-label>-->
+      <!--              <q-item-label-->
+      <!--                caption-->
+      <!--                class="row justify-end">-->
+      <!--                <small>{{ task.dueTime }}</small>-->
+      <!--              </q-item-label>-->
+      <!--            </div>-->
+      <!--          </div>-->
+      <!--        </q-item-section>-->
+      <!--      </q-item>-->
     </q-list>
   </q-page>
 </template>
 
 <script>
 import {mapGetters} from 'vuex'
+
 export default {
   computed: {
-   ...mapGetters('tasks',['tasks'])
+    ...mapGetters('tasks', ['tasks'])
+  },
+  components: {
+    'task': require('components/Tasks/Task.vue').default
   }
 }
 
--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -1,44 +1,40 @@
 const state = {
-    tasks: [
-        {
-            id: 1,
-            name: "go to shopping",
-            completed: false,
-            dueDate: '2020/09/12',
-            dueTime: '8:30'
-        },
-        {
-            id: 2,
-            name: "get bananas",
-            completed: false,
-            dueDate: '2020/09/13',
-            dueTime: '14:30'
-        },
-        {
-            id: 3,
-            name: "get apples",
-            completed: false,
-            dueDate: '2020/09/14',
-            dueTime: '16:30'
-        },
-
-    ]
-
+  tasks: {
+    'ID1': {
+      id: 1,
+      name: "go to shopping",
+      completed: false,
+      dueDate: '2020/09/12',
+      dueTime: '8:30'
+    },
+    'ID2': {
+      id: 2,
+      name: "get bananas",
+      completed: false,
+      dueDate: '2020/09/13',
+      dueTime: '14:30'
+    },
+    'ID3': {
+      id: 3,
+      name: "get apples",
+      completed: false,
+      dueDate: '2020/09/14',
+      dueTime: '16:30'
+    }
+  }
 }
 
-const mutations = {
+const mutations = {}
 
-}
-
-const getters ={
-tasks: (state)=>{
+const getters = {
+  tasks: (state) => {
     return state.tasks
-}
+  }
 }
 
 export default {
-    namespaced:true,
-    state,
-    mutations,
-    getters
+  namespaced: true,
+  state,
+  mutations,
+  getters
 }
