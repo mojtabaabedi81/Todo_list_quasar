d5f1de97591a125b02bba0fe29819675297f211e
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2024-03-05 10:10:41 +0330

    fix the bug of the unexpected mutation and the error that don't let to show the page

--- src/components/Tasks/TaskItem.vue
+++ src/components/Tasks/TaskItem.vue
@@ -3,37 +3,25 @@
     @click="toggleCompleted"
     :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
     clickable
-    v-ripple>
+    v-ripple
+  >
     <q-item-section side top>
-      <q-checkbox v-model="task.completed"/>
+      <q-checkbox :value="task.completed" @input="toggleCompleted"/>
     </q-item-section>
     <q-item-section>
-      <q-item-label
-        :class="{'text-strikethrough' : task.
-            completed}"
-      >{{ task.name }} {{id}}
+      <q-item-label :class="{'text-strikethrough' : task.completed}">
+        {{ task.name }} {{ id }}
       </q-item-label>
     </q-item-section>
     <q-item-section side>
       <div class="row">
         <div class="column justify-center">
-          <q-icon
-            name="event"
-            size="18px"
-            class="q-mr-xs"
-          />
+          <q-icon name="event" size="18px" class="q-mr-xs"/>
         </div>
 
         <div class="column">
-          <q-item-label
-            class="row justify-end"
-            caption>{{ task.dueDate }}
-          </q-item-label>
-          <q-item-label
-            caption
-            class="row justify-end">
-            <small>{{ task.dueTime }}</small>
-          </q-item-label>
+          <q-item-label class="row justify-end" caption>{{ task.dueDate }}</q-item-label>
+          <q-item-label caption class="row justify-end"><small>{{ task.dueTime }}</small></q-item-label>
         </div>
       </div>
     </q-item-section>
